6e7984ebd7538c5b5169bb1d50519ede92afd974
cnrdmrci <canerdemirci.mail@gmail.com>
2021-02-18 21:42:20 +0300

    Modeller eklendi.

---
+++ Model/Item.cs
@@ -0,0 +1,8 @@
+namespace Model
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Model/Person.cs
@@ -0,0 +1,9 @@
+namespace Model
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ItemId { get; set; }
+    }
+}
